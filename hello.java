a97056d5d7d0d19b45e3c5527636b680d3a0a033
pankaj996699 <110814495+pankaj996699@users.noreply.github.com>
2022-08-08 14:19:09 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CPLAb-50
+ */
+public class hello {
+    
+    public static void main(string args[])
+    {
+        System.out.println("Hello world");
+    }
+    
+}
